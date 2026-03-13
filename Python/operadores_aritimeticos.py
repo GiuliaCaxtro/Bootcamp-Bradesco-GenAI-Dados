67cc9195087318f3f92a0469957671bfd512a6f0
Giulia Castro <giuliareis21@gmail.com>
2026-03-13 19:34:30 -0300

    Create operadores_aritimeticos.py

---
+++ Python/operadores_aritimeticos.py
@@ -0,0 +1,22 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2) #adição
+print(produto_1 - produto_2) #subtração  
+print(produto_1 * produto_2) #multiplicação
+print(produto_1 / produto_2) #divisão
+print(produto_1 // produto_2) #divisão inteira
+print(produto_1 % produto_2) #resto da divisão
+print(produto_1 ** produto_2) #potenciação
+
+
+x = 10 + 5 * 4
+y = (10 + 5) * 4
+print(x) #ordem de precedência dos operadores
+print(y) #ordem de precedência dos operadores
+
+#ordem de precedência dos operadores
+#1. Parênteses ()
+#2. Exponenciação (**)
+#3. Multiplicação (*) e Divisão (/, //, %)
+#4. Adição (+) e Subtração (-)
