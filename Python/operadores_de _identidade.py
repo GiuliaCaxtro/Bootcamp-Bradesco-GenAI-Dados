7693a6459ee097764ebecf8889bb724fc3f4fcb8
Giulia Castro <giuliareis21@gmail.com>
2026-03-13 22:31:29 -0300

    .

---
+++ Python/operadores_de _identidade.py
@@ -0,0 +1,4 @@
+saldo = 1000
+limite = 1000
+print(saldo is limite)  # Verifica se saldo e limite são o mesmo objeto na memória
+print(saldo is not limite)  # Verifica se saldo e limite são objetos diferentes na memória
---
+++ Python/operadores_de_associacao.py
@@ -0,0 +1,7 @@
+curso = 'curso de python'
+frutas = ['maçã', 'banana', 'laranja']
+saque = [100, 200, 300]
+
+print('python' in curso)  # Verifica se a string 'python' está presente na variável curso
+print('maçã' in frutas)  # Verifica se a string 'maçã' está presente na lista frutas
+print('maçã' not in frutas)  # Verifica se a string 'maçã' não está presente na lista frutas
